a9a75651f37054379a04ed6f74769a8514106263
hnxlea <58263800+hnxlea@users.noreply.github.com>
2022-05-11 07:29:38 +0200

    Update deploy-docker-compose.sh

--- deploy-docker-compose.sh
+++ deploy-docker-compose.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+docker run -dti --restart always --name container-web-tty \
+    -p 80:8080 \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    wrfly/container-web-tty
+
+# tail logs
+docker logs -f container-web-tty
